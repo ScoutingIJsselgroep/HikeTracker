38a405266fd7f78b13897848ac500d4fcb5bb6d1
Tristan de Boer <tristandeboer@nac34470.local>
2020-06-29 17:53:13 +0200

    Removed db.sqlite

--- Screen/admin.py
+++ Screen/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Checkpoint, Team
+from .models import Checkpoint, Team, Visit
 
 admin.site.register(Checkpoint)
 admin.site.register(Team)
+admin.site.register(Visit)
