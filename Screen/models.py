36a4ad90420b481e5c0463bf920ddac7501cb26f
Tristan de Boer <info@tristandeboer.nl>
2021-05-11 11:22:27 +0200

    Add route to model

--- Screen/models.py
+++ Screen/models.py
@@ -6,13 +6,21 @@ def random_string(stringLength=30):
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
 
-# Create your models here.
+
+class Route(models.Model):
+    name = models.CharField('Name', max_length=200, default='')
+    uuid = models.CharField(primary_key=True, default=random_string, editable=False, max_length=200)
+    description = models.TextField('Description', max_length=2000, blank=True)
+
+    def __str__(self):
+        return self.name
 class Checkpoint(models.Model):
     uuid = models.CharField(primary_key=True, default=random_string, editable=True, max_length=200)
     name = models.CharField('Name', max_length=200)
     puzzle = models.URLField('Puzzle', max_length=200, blank=True)
     location = models.CharField('Location', max_length=200, blank=True)
     description = models.TextField('Description', max_length=2000, blank=True)
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.name
@@ -20,6 +28,7 @@ class Checkpoint(models.Model):
 class Team(models.Model):
     name = models.CharField('Name', max_length=200)
     uuid = models.CharField(primary_key=True, default=random_string, editable=True, max_length=200)
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.name
