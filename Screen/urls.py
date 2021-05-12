709d4296c8b267f36a1743faddd69aae955131f2
Tristan de Boer <info@tristandeboer.nl>
2021-05-12 19:14:26 +0200

    Fix name

--- Screen/urls.py
+++ Screen/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('checkpoint/<slug:slug>', views.checkpoint, name='secret'),
     path('register/<slug:slug>', views.session, name='session'),
-    path('progress', views.progress, name='session')
+    path('progress', views.progress, name='progress')
 ]
