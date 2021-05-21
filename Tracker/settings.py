6a72e2251229f334019084680c63f28abff89a98
Tristan de Boer <info@tristandeboer.nl>
2021-05-21 15:09:50 +0200

    Disable safe mode

--- Tracker/settings.py
+++ Tracker/settings.py
@@ -118,6 +118,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+MARKUP_SETTINGS = {
+    'markdown': {
+        'safe_mode': False
+    }
+}
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
