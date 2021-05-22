401b4ed7042f81822e0a86f6f2db156ed8c74c2c
Tristan de Boer <info@tristandeboer.nl>
2021-05-22 12:06:18 +0200

    Add QR downloader

--- Screen/urls.py
+++ Screen/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('checkpoint/<slug:slug>', views.checkpoint, name='secret'),
     path('checkpoints', views.checkpoints, name='checkpoints'),
     path('register/<slug:slug>', views.session, name='session'),
-    path('progress', views.progress, name='progress')
+    path('route', views.route, name='route'),
+    path('progress', views.progress, name='progress'),
+    path('generateqr/<slug:route_id>', views.generate_qr, name='generate_qr'),
 ]
--- Screen/views.py
+++ Screen/views.py
@@ -1,9 +1,16 @@
-from .models import Checkpoint, Team, Visit
+from .models import Checkpoint, Team, Visit, Route
 
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.response import TemplateResponse
+
+from reportlab.pdfgen import canvas
+from reportlab.graphics.shapes import Drawing 
+from reportlab.graphics.barcode.qr import QrCodeWidget 
+from reportlab.graphics import renderPDF
+from reportlab.lib.units import cm
+
 
 import datetime
 
@@ -31,6 +38,21 @@ def session(request, slug):
     response.set_cookie('registration', slug)
 
     return response 
+
+def route(request):
+    if 'registration' not in request.COOKIES:
+        return render(request, "not_registered.html")
+    team = Team.objects.get(uuid=request.COOKIES['registration'])
+
+    # Get route identifier
+    route_id = Team.objects.get(uuid=request.COOKIES['registration']).route
+
+    # Get route
+    route = Route.objects.get(uuid=route_id.uuid)
+
+    # Render page
+    return render(request, "route.html", {"route": route, "team": team, 'isadmin': request.user.is_authenticated})
+
 
 def checkpoint(request, slug):
     if 'registration' not in request.COOKIES:
@@ -74,6 +96,7 @@ def progress(request):
     route_id = Team.objects.get(uuid=request.COOKIES['registration']).route
 
     # Generate matrix
+    current_team = Team.objects.get(uuid=request.COOKIES['registration']) 
     teams = Team.objects.filter(route=route_id).all()
     checkpoints = Checkpoint.objects.filter(route=route_id).order_by('name')
     progress = [{"name": team.name, "uuid": team.uuid, "checkpoints": [{"name": checkpoint.name, "uuid": checkpoint.uuid, "visited": False} for checkpoint in checkpoints]} for team in teams]
@@ -88,4 +111,63 @@ def progress(request):
                         checkpoint["visited"] = True
                         checkpoint["date_visited"] = visit.date_visited
 
-    return render(request, "progress.html", {'progress': progress})
+    return render(request, "progress.html", {'progress': progress, 'team': current_team})
+
+def generate_qr(request, route_id):
+    if not request.user.is_authenticated:
+        return render(request, "not_registered.html")
+        
+    route = Route.objects.get(uuid=route_id)
+    # Get routes
+    checkpoints = Checkpoint.objects.filter(route=route_id).order_by('name')
+
+    # Get teams
+    teams = Team.objects.filter(route=route_id).all()
+
+    # Create the HttpResponse object with the appropriate PDF headers.
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="{route.name}.pdf"'
+
+    p = canvas.Canvas(response)
+
+    for checkpoint in checkpoints:
+        qrw = QrCodeWidget(f"https://piraat-hike-tracker.herokuapp.com/register/{checkpoint.uuid}") 
+        b = qrw.getBounds()
+        qrw.barHeight = 20*cm
+        qrw.barWidth = 20*cm
+
+        d = Drawing(20*cm, 20*cm) 
+        d.add(qrw)
+        p.setFont('Helvetica',30)
+        p.drawCentredString(21*cm/2, 24*cm, "CHECKPOINT")
+        p.setFont('Helvetica',45)
+        p.drawCentredString(21*cm/2, 22*cm, checkpoint.name)
+        p.setFont('Helvetica',20)
+        p.drawCentredString(21*cm/2, 20*cm, f"QR code voor {route.name}. S.v.p. laten hangen.")
+
+        renderPDF.draw(d, p, 0, 0)
+        
+        p.showPage()
+    
+    for team in teams:
+        qrw = QrCodeWidget(f"https://piraat-hike-tracker.herokuapp.com/register/{team.uuid}") 
+        b = qrw.getBounds()
+
+        qrw.barHeight = 10*cm
+        qrw.barWidth = 10*cm
+
+        d = Drawing(10*cm, 10*cm) 
+        d.add(qrw)
+        p.setFont('Helvetica',30)
+        p.drawCentredString(21*cm/2, 24*cm, "TEAM")
+        p.setFont('Helvetica',45)
+        p.drawCentredString(21*cm/2, 22*cm, team.name)
+        p.setFont('Helvetica',20)
+        p.drawCentredString(21*cm/2, 20*cm, f"Scan mij om je telefoon te registreren!")
+
+        renderPDF.draw(d, p, 5*cm, 0)
+        
+        p.showPage()
+    
+    p.save()
+    return response
